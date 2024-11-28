75ea5057130a9283154779d5bd1f62b0bfd405d5
JosueHenriquez <52579357+JosueHenriquez@users.noreply.github.com>
2024-11-28 16:19:22 -0600

    Proyecto inicial

---
+++ Curso/src/main/java/CursoJava/Curso/CursoApplication.java
@@ -0,0 +1,13 @@
+package CursoJava.Curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //anotación
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
---
+++ Curso/src/test/java/CursoJava/Curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package CursoJava.Curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
